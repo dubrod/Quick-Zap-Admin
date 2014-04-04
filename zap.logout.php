9d9ef5b5d5f43eb78bafc98ab88c0930e9e79c6c
dubrod <dubrod@icloud.com>
2014-04-04 08:48:15 -0400

    Logout Config

--- zap.logout.php
+++ zap.logout.php
@@ -2,12 +2,14 @@
 # R3Login Lib - Logout.php
 # Version 2.0
 # March 15 2014 - Year of our Lord
-   		
+
+	require_once 'lib/config.php';
+	   		
 	unset($user);
 	session_start();
 	session_unset();
 	session_destroy();
 	
-	header('Location: /intx-admin');	//logout and send back to index 
+	header('Location: '.ADMIN_BASE_URL.'');	
        	
 ?>       	
